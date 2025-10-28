d742bb8eb4d10baa3656db1fac4930fdcde20368
malog <2153315236@qq.com>
2025-10-28 21:57:12 +0800

    feat: 添加简单的本地文件记忆功能

--- internal/handler/app_handler.py
+++ internal/handler/app_handler.py
@@ -5,11 +5,16 @@
 """
 import uuid
 from dataclasses import dataclass
+from operator import itemgetter
 
 from injector import inject
 from uuid import UUID
+
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import ChatOpenAI
 
 from internal.exception import FailException
@@ -57,18 +62,34 @@ class AppHandler:
         # 创建聊天提示模板，使用系统提示词和用户查询
         prompt = ChatPromptTemplate.from_messages([
             ("system", system),
+            MessagesPlaceholder("history"),
             ("user", "{query}")
         ])
+
+        # 创建一个ConversationBufferWindowMemory实例，用于管理对话历史记录，该内存管理器只保留最近的k条对话记录，避免内存无限增长
+        memory = ConversationBufferWindowMemory(
+            k=3,
+            input_key="query",
+            output_key="output",
+            return_messages=True,
+            chat_memory=FileChatMessageHistory("./storage/memory/chat_history.txt"),
+        )
+
         # 初始化ChatOpenAI模型，指定使用qwen3-max模型
         llm = ChatOpenAI(model="qwen3-max")
+
         # 创建字符串输出解析器，用于解析模型输出
         parser = StrOutputParser()
 
         # 构建处理链：提示模板 -> LLM模型 -> 输出解析器
-        chain = prompt | llm | parser
+        chain = RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        ) | prompt | llm | parser
 
         # 执行链式调用，传入查询数据并获取处理结果
-        content = chain.invoke({"query": req.query.data})
+        chain_input = {"query": req.query.data}
+        content = chain.invoke(chain_input)
+        memory.save_context(chain_input, {"output": content})
 
         # 返回成功响应
         return success_json({"content": content})
